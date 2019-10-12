0ecdd663f6e61bdee193c2bf9157714531692834
Veronika <veronimakoroni@gmail.com>
2019-10-12 10:27:52 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var tvins=0,fives=0,zeros=0,inc=2,start=1;
+    var ex = expression.split('*');
+    for (var step=0;step<ex.length;step++){
+        var num = parseInt(ex[step]);
+        if (ex[step].indexOf('!!') != -1){                
+            inc=2;
+            if (num % 2 == 1){                
+                start=3; 
+            } else {                
+                start=2; 
+            }
+        } else {            
+            inc=1; 
+            start=2;
+        }
+        
+        for (var i=start;i<=num;i+=inc){
+            var n = i;
+            while (n%10 == 0){
+                zeros ++;
+                n /= 10;
+            }
+
+            while (n%5 == 0){
+                fives ++;
+                n /= 5;
+            }
+
+            while (n%2 == 0){
+                tvins ++;
+                n /= 2;
+            }                
+        }
+    }
+
+    return zeros+Math.min(tvins,fives); 
 }
